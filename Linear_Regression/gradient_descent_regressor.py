6949e86386f80ebfd4d2e18e30834a8f9ff7e113
priyanshgoantiya <161962313+priyanshgoantiya@users.noreply.github.com>
2025-02-10 22:21:08 +0530

    Create gradient_descent_regressor.py

---
+++ Linear_Regression/gradient_descent_regressor.py
@@ -0,0 +1,26 @@
+# gradient_descent_regressor
+import numpy 
+class gradient_descent_regressor:
+  def __init__(self,learning_rate,epochs):
+    self.m=100
+    self.b=-120
+    self.lr=learning_rate
+    self.iteration=epochs
+  def fit(self,X,y):
+    ## clac the b by gd
+    for i in range(self.iteration):
+      X=X.ravel()
+      loss_slope_b=-2 * np.sum(y -self.m*X - self.b)
+      self.b=self.b-(loss_slope_b*self.lr)
+      loss_slope_m=-2 * np.sum((y -self.m*X - self.b)*X)
+      self.m=self.m-(loss_slope_m*self.lr)
+      # print(loss_slope,self.b)
+    print(self.b,self.m)
+  def predict(self,X):
+    X=X.ravel()
+    y_pred=self.m*X + self.b
+    return y_pred
+  def r2_score(self, y_test, y_pred):
+    ssr = np.sum((y_test - y_pred) ** 2)
+    ssm = np.sum((y_test - np.mean(y_test)) ** 2)
+    return 1 - (ssr / ssm)
