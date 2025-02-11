a948eac60f90725e5d8ebb10deec6a4332949c4e
priyanshgoantiya <161962313+priyanshgoantiya@users.noreply.github.com>
2025-02-12 01:05:57 +0530

    Update and rename Batch_gradient_descent_regressor.py to Batch_gradient_descent_regressor_lr_scheduling.py

--- Linear_Regression/Batch_gradient_descent_regressor_lr_scheduling.py
+++ Linear_Regression/Batch_gradient_descent_regressor_lr_scheduling.py
@@ -1,4 +1,5 @@
-# Batch_gradient_descent_regressor
+
+# Batch_gradient_descent_regressor_lr_scheduling
 import numpy 
 class Batch_gradient_descent_regressor:
   def __init__(self,learning_rate=0.01,epochs=100):
@@ -6,17 +7,22 @@ class Batch_gradient_descent_regressor:
     self.intercept_ = None
     self.lr=learning_rate
     self.iteration=epochs
+    self.t0=5
+    self.t1=50
+  def learning_rate_scheduling(self,t):
+    return self.t0/np.sqrt(self.t1+t)
   def fit(self,X_train,y_train):
     n_samples, n_features = X_train.shape
     self.coef_=np.random.randn(n_features)
     self.intercept_=np.random.randn()
     for i in range(self.iteration):
       # update all coef and the intercept
+      lr=self.learning_rate_scheduling(i)
       y_hat=np.dot(X_train,self.coef_) + self.intercept_
       slope_intercept=(-2/n_samples) * np.sum(y_train-y_hat)
-      self.intercept_=self.intercept_ - (self.lr * slope_intercept)
+      self.intercept_=self.intercept_ - (lr * slope_intercept)
       slope_coef=(-2/n_samples) * np.dot((y_train-y_hat),X_train)
-      self.coef_=self.coef_ - (self.lr * slope_coef)
+      self.coef_=self.coef_ - (lr * slope_coef)
   def predict(self,X_test):
     y_pred=np.dot(X_test,self.coef_) + self.intercept_
     return y_pred
