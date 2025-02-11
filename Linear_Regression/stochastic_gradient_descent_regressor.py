df4a515daea391f144e796fed4516d6c852efbf5
priyanshgoantiya <161962313+priyanshgoantiya@users.noreply.github.com>
2025-02-11 21:30:53 +0530

    Create stochastic_gradient_descent_regressor.py

---
+++ Linear_Regression/stochastic_gradient_descent_regressor.py
@@ -0,0 +1,28 @@
+# stochastic_gradient_descent_regressor
+import numpy 
+class stochastic_gradient_descent_regressor:
+  def __init__(self,learning_rate=0.01,epochs=100):
+    self.coef_ = None
+    self.intercept_ = None
+    self.lr=learning_rate
+    self.iteration=epochs
+  def fit(self,X_train,y_train):
+    n_samples, n_features = X_train.shape
+    self.coef_=np.random.randn(n_features)
+    self.intercept_=np.random.randn()
+    for i in range(self.iteration):
+      for j in range(n_samples):
+        index=np.random.randint(n_samples)
+        # update all coef and the intercept
+        y_hat=np.dot(X_train[index],self.coef_) + self.intercept_
+        slope_intercept=(-2) * (y_train[index]-y_hat)
+        self.intercept_=self.intercept_ - (self.lr * slope_intercept)
+        slope_coef=(-2) * np.dot((y_train[index]-y_hat),X_train[index])
+        self.coef_=self.coef_ - (self.lr * slope_coef)
+  def predict(self,X_test):
+    y_pred=np.dot(X_test,self.coef_) + self.intercept_
+    return y_pred
+  def r2_score(self, y_test, y_pred):
+    ssr = np.sum((y_test - y_pred) ** 2)
+    ssm = np.sum((y_test - np.mean(y_test)) ** 2)
+    return 1 - (ssr / ssm)
