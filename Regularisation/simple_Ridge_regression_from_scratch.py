a366cc089b521efe33d84a6cf8a87ec63d22ca71
priyanshgoantiya <161962313+priyanshgoantiya@users.noreply.github.com>
2025-02-23 01:33:20 +0530

    Create simple_Ridge_regression_from_scratch.py

---
+++ Regularisation/simple_Ridge_regression_from_scratch.py
@@ -0,0 +1,17 @@
+## simple_Ridge_regression_from_scratch
+import numpy as np
+class Ridge_regression_from_scratch:
+  def __init__(self,alpha=0.01):
+    self.alpha=alpha
+    self.m=None
+    self.b=None
+  def fit(self,X_train,y_train):
+    num=0
+    den=0
+    for i in range(len(X_train)):
+      num=num + (y_train[i]-np.mean(y_train)) * (X_train[i]-np.mean(X_train))
+      den= den + (X_train[i]-np.mean(X_train)) * (X_train[i]-np.mean(X_train))
+    self.m=num/(den+ self.alpha)
+    self.b=np.mean(y_train)-[(self.m)*np.mean(X_train)]
+  def predict(self,X_test):
+    return np.array([self.m*x + self.b for x in X_test])
