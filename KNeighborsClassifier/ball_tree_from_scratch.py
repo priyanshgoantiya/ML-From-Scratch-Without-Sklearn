20f37259cdf8f78eeb2d982f18948d00d7a25a25
priyanshgoantiya <161962313+priyanshgoantiya@users.noreply.github.com>
2025-02-28 15:37:06 +0530

    Create ball_tree_from_scratch.py

---
+++ KNeighborsClassifier/ball_tree_from_scratch.py
@@ -0,0 +1,21 @@
+##ball_tree_from_scratch
+from scipy.spatial import cKDTree
+import numpy 
+class ball_tree_from_scratch:
+  def __init__(self,K):
+    self.neighbors=K
+    self.X_train=None
+    self.y_train=None
+    self.tree=None
+  def fit(self,X_train,y_train):
+    self.X_train=np.array(X_train)
+    self.y_train=np.array(y_train)
+    self.tree=cKDTree(X_train)
+  def predict(self,X_test):
+    prediction=[]
+    X_test = np.array(X_test)
+    for i in range(len(X_test)):
+      distances,indices=self.tree.query(X_test[i],k=self.neighbors)
+      majority_count=np.bincount(self.y_train[indices]).argmax()
+      prediction.append(majority_count)
+    return prediction
