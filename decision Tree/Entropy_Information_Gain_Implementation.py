c1c9f0c77844aa05462cb82b449295e3139d2d1e
Priyansh Goantiya <161962313+priyanshgoantiya@users.noreply.github.com>
2025-05-14 20:44:42 +0530

    Update Entropy_Information_Gain_Implementation.py

--- decision Tree/Entropy_Information_Gain_Implementation.py
+++ decision Tree/Entropy_Information_Gain_Implementation.py
@@ -22,10 +22,17 @@ def Information_gain(data,feature_index):
     weight=len(subset)/len(data)
     entropy_after_split+=(weight*subset_entropy)
   return entropy_before_split-entropy_after_split
-data = [['Sunny', 85, 85, False, 'No'],
-        ['Sunny', 80, 90, True, 'No'],
-        ['Overcast', 83, 86, False, 'Yes'],
-        ['Rainy', 70, 96, False, 'Yes'],
-        ['Rainy', 68, 80, False, 'Yes']]
+data = [
+    ['Sunny', 85, 85, False, 'High', 'Weak', 'No', 5.5, 'Mild', 'Dry', 'No'],
+    ['Sunny', 80, 90, True, 'High', 'Weak', 'Yes', 6.0, 'Hot', 'Humid', 'No'],
+    ['Overcast', 83, 86, False, 'High', 'Strong', 'No', 5.0, 'Mild', 'Dry', 'Yes'],
+    ['Rainy', 70, 96, False, 'Normal', 'Strong', 'Yes', 4.5, 'Cool', 'Wet', 'Yes'],
+    ['Rainy', 68, 80, False, 'Normal', 'Weak', 'No', 5.8, 'Cool', 'Humid', 'Yes'],
+    ['Rainy', 65, 70, True, 'Normal', 'Weak', 'Yes', 6.2, 'Cool', 'Humid', 'No'],
+    ['Overcast', 64, 65, True, 'High', 'Strong', 'No', 6.4, 'Mild', 'Dry', 'Yes'],
+    ['Sunny', 72, 95, False, 'High', 'Weak', 'No', 5.1, 'Hot', 'Humid', 'No'],
+    ['Sunny', 69, 70, False, 'Normal', 'Strong', 'Yes', 4.9, 'Cool', 'Dry', 'Yes'],
+    ['Rainy', 75, 80, False, 'Normal', 'Strong', 'No', 6.3, 'Mild', 'Wet', 'Yes']
+]
 IG=Information_gain(data,3)
 print(IG)
